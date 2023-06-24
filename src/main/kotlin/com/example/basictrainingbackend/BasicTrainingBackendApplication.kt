add80a81951aa0858a99f4ce795a21b0bfff52a4
Jaewook Choi <eari100@naver.com>
2023-06-24 16:25:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/basictrainingbackend/BasicTrainingBackendApplication.kt
@@ -0,0 +1,12 @@
+package com.example.basictrainingbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicTrainingBackendApplication
+
+fun main(args: Array<String>) {
+
+    runApplication<BasicTrainingBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/basictrainingbackend/BasicTrainingBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.basictrainingbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicTrainingBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
