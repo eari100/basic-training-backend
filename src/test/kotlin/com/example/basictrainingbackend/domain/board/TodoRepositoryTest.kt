1b16c74b99dd36dc96fff7c84ac8a7d3e9e43437
Jaewook Choi <eari100@naver.com>
2023-06-24 18:49:50 +0900

    test: Todo JpaRepository 삭제 테스트

--- src/test/kotlin/com/example/basictrainingbackend/domain/board/TodoRepositoryTest.kt
+++ src/test/kotlin/com/example/basictrainingbackend/domain/board/TodoRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.example.basictrainingbackend.domain.board
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -25,7 +26,7 @@ class TodoRepositoryTest {
 
     @Test
     fun testUpdateTodo() {
-// Given
+        // Given
         val todo = Todo(0, "게임하기")
 
         // When
@@ -40,5 +41,22 @@ class TodoRepositoryTest {
         // Then
         val updatedTodo = todoRepository.findById(todoId).orElse(null)
         assertEquals(updatedTodo?.item, "공부하기")
+    }
+
+    @Test
+    fun testDeleteTodo() {
+        // Given
+        val todo = Todo(0, "숙제하기")
+        val todoId = todo.id
+
+        // When
+        val savedTodo = todoRepository.save(todo)
+
+        // When
+        todoRepository.deleteById(savedTodo.id)
+
+        // Then
+        val deletedTodo = todoRepository.findById(todoId).orElse(null)
+        assertNull(deletedTodo)
     }
 }
